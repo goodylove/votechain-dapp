6edd6a9f788ec3129046c66664d2500e6dae7042
goodylove <goodyc474@gmail.com>
2026-01-03 22:58:37 +0100

    chore: update dependencies in package.json
    - Added @reown/appkit, @reown/appkit-adapter-wagmi, @tanstack/react-query, postcss, viem, and wagmi.
    - Updated @tailwindcss/postcss and tailwindcss to version 4.1.18.

---
+++ context/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { type ReactNode } from "react";
+
+import { projectId, wagmiAdapter } from "@/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAppKit, CreateAppKit } from "@reown/appkit/react";
+import { mainnet } from "@reown/appkit/networks";
+import { WagmiProvider, cookieToInitialState, type Config } from "wagmi";
+
+// set up react-query client
+const queryClient = new QueryClient();
+
+if (!projectId) {
+  throw new Error("ProjectId is not defined");
+}
+
+// Create the modal
+
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks: [mainnet],
+  defaultNetwork: mainnet,
+  features: {
+    analytics: true,
+  },
+});
+
+function ContextProvider({
+  children,
+  cookies,
+}: {
+  children: ReactNode;
+  cookies?: string | null;
+}) {
+  const initialState = cookieToInitialState(
+    wagmiAdapter.wagmiConfig as Config,
+    cookies
+  );
+  return (
+    <WagmiProvider
+      initialState={initialState}
+      config={wagmiAdapter.wagmiConfig as Config}
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default ContextProvider;
